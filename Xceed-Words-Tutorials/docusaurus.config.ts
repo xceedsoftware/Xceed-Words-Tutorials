2c2862f2d06b02c6f44172ba038aa6656b0e9b42
mattd1980 <support@xceed.com>
2024-07-24 13:10:47 -0400

    fixed the website

--- Xceed-Words-Tutorials/docusaurus.config.ts
+++ Xceed-Words-Tutorials/docusaurus.config.ts
@@ -7,23 +7,16 @@ const config: Config = {
   tagline: 'Learn how to use Xceed Words for .NET effectively',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://xceedsoftware.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Xceed-Words-Tutorials/',
+  trailingSlash: true,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'xceedsoftware', // Usually your GitHub org/user name.
-  projectName: 'Xceed-Words-Tutorials', // Usually your repo name.
+  organizationName: 'xceedsoftware',
+  projectName: 'Xceed-Words-Tutorials',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -36,14 +29,10 @@ const config: Config = {
         docs: {
           routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: require.resolve('./sidebars.ts'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/xceedsoftware/Xceed-Words-Tutorials/edit/main/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl: 'https://github.com/xceedsoftware/Xceed-Words-Tutorials/edit/main/blog/',
         },
         theme: {
@@ -54,7 +43,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Xceed Words for .NET Tutorials',

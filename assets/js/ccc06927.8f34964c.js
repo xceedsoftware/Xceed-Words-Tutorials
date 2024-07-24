68d3274b0b6bdd074d4e3f1d05ae547bdc27e697
xceedsoftware <xceedsoftware@users.noreply.github.com>
2024-07-24 18:20:33 +0000

    deploy: ab6828d47ff427859c2f96d3ab930e3a73781b70

---
+++ assets/js/ccc06927.8f34964c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkxceed_words_tutorials=self.webpackChunkxceed_words_tutorials||[]).push([[470],{3996:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Xceed-Words-Tutorials/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
